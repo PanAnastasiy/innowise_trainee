fba9b26b71d8fbac5a8e41b9372591d20f2e945d
Your Name <youremail@example.com>
2025-10-25 03:15:10 +0300

    task-3

---
+++ task-3/consts.py
@@ -0,0 +1,3 @@
+
+DF_CSV_PATH = '../resources/csv/adult.data.csv'
+NUMPY_CSV = '../resources/csv/cereal.csv'
---
+++ utils/design/beauty.py
@@ -0,0 +1,11 @@
+
+class Color:
+    RESET = "\u001B[0m"
+    RED = "\u001B[31m"
+    GREEN = "\u001B[32m"
+    YELLOW = "\u001B[33m"
+    BLUE = "\u001B[34m"
+    PURPLE = "\u001B[35m"
+    CYAN = "\u001B[36m"
+    LIGHT_WHITE = "\u001B[97m"
+    BOLD = "\u001B[1m"
