508ac9cc4dd0fda5d3d4dc5999f382d1a805dd4a
Your Name <youremail@example.com>
2025-10-25 22:24:08 +0300

    task-4-final-review

--- TASKS/task-4/cli_main.py
+++ TASKS/task-4/cli_main.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import sys
 
-# Правильно определяем корень проекта
+# Не видит папки за рамками задания, приходится подключать корень проекта
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if PROJECT_ROOT not in sys.path:
     sys.path.append(PROJECT_ROOT)
